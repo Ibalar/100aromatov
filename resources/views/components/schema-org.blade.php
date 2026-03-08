211766ae2952e65aeb9ebf8f3bc558f7e4349a3f
Ibalar <ibalartutby@gmail.com>
2026-03-08 20:58:07 +0300

    #2_08.03.26 update Views

--- resources/views/components/schema-org.blade.php
+++ resources/views/components/schema-org.blade.php
@@ -9,76 +9,86 @@
 ])
 
 @if($type === 'brand')
-<script type="application/ld+json">
-{
-    "@context": "https://schema.org",
-    "@type": "Brand",
-    "name": "{{ $entity->name }}",
-    "description": "{{ localizedField($entity, 'description') }}",
-    "url": "{{ route('brand.show', $entity->slug) }}"
-    @if($entity->logo)
-    ,"logo": "{{ asset('storage/' . $entity->logo) }}"
-    @endif
-}
-</script>
+    @php
+        $schema = [
+            '@context' => 'https://schema.org',
+            '@type' => 'Brand',
+            'name' => $entity->name,
+            'description' => localizedField($entity, 'description'),
+            'url' => route('brand.show', $entity->slug),
+        ];
+
+        if($entity->logo) {
+            $schema['logo'] = asset('storage/' . $entity->logo);
+        }
+    @endphp
+    <script type="application/ld+json">
+        {!! json_encode($schema, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) !!}
+    </script>
 @elseif($type === 'brand_products')
-<script type="application/ld+json">
-{
-    "@context": "https://schema.org",
-    "@type": "CollectionPage",
-    "name": "{{ $title }}",
-    "description": "{{ $description }}",
-    "url": "{{ request()->url() }}",
-    "numberOfItems": {{ $products->total() }},
-    "itemListElement": [
-        @foreach($products as $index => $product)
-        @if($index > 0),@endif
-        {
-            "@type": "ListItem",
-            "position": {{ $index + 1 }},
-            "item": {
-                "@type": "Product",
-                "name": "{{ localizedField($product, 'name') }}",
-                "url": "{{ route('product.show', $product->slug) }}",
-                "brand": {
-                    "@type": "Brand",
-                    "name": "{{ $product->brand->name }}"
-                },
-                "offers": {
-                    "@type": "Offer",
-                    "price": "{{ $product->min_price_usd }}",
-                    "priceCurrency": "USD",
-                    "availability": "https://schema.org/InStock"
-                }
-            }
+    @php
+        $items = [];
+        foreach($products as $index => $product) {
+            $items[] = [
+                '@type' => 'ListItem',
+                'position' => $index + 1,
+                'item' => [
+                    '@type' => 'Product',
+                    'name' => localizedField($product, 'name'),
+                    'url' => route('product.show', $product->slug),
+                    'brand' => [
+                        '@type' => 'Brand',
+                        'name' => $product->brand->name
+                    ],
+                    'offers' => [
+                        '@type' => 'Offer',
+                        'price' => $product->min_price_usd,
+                        'priceCurrency' => 'USD',
+                        'availability' => 'https://schema.org/InStock'
+                    ]
+                ]
+            ];
         }
-        @endforeach
-    ]
-}
-</script>
+
+        $schema = [
+            '@context' => 'https://schema.org',
+            '@type' => 'CollectionPage',
+            'name' => $title,
+            'description' => $description,
+            'url' => request()->url(),
+            'numberOfItems' => $products->total(),
+            'itemListElement' => $items
+        ];
+    @endphp
+    <script type="application/ld+json">
+        {!! json_encode($schema, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) !!}
+    </script>
 @elseif($type === 'brands_list')
-<script type="application/ld+json">
-{
-    "@context": "https://schema.org",
-    "@type": "CollectionPage",
-    "name": "{{ $title }}",
-    "description": "{{ $description }}",
-    "url": "{{ request()->url() }}",
-    "numberOfItems": {{ $count }},
-    "itemListElement": [
-        @foreach($brands as $index => $brand)
-        @if($index > 0),@endif
-        {
-            "@type": "ListItem",
-            "position": {{ $index + 1 }},
-            "item": {
-                "@type": "Brand",
-                "name": "{{ $brand->name }}",
-                "url": "{{ route('brand.show', $brand->slug) }}"
-            }
+    @php
+        $items = [];
+        foreach($brands as $index => $brand) {
+            $items[] = [
+                '@type' => 'ListItem',
+                'position' => $index + 1,
+                'item' => [
+                    '@type' => 'Brand',
+                    'name' => $brand->name,
+                    'url' => route('brand.show', $brand->slug)
+                ]
+            ];
         }
-        @endforeach
-    ]
-}
-</script>
+
+        $schema = [
+            '@context' => 'https://schema.org',
+            '@type' => 'CollectionPage',
+            'name' => $title,
+            'description' => $description,
+            'url' => request()->url(),
+            'numberOfItems' => $count,
+            'itemListElement' => $items
+        ];
+    @endphp
+    <script type="application/ld+json">
+        {!! json_encode($schema, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) !!}
+    </script>
 @endif
