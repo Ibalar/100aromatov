fa4193b331bdc9b8ee3b0dda1fb165208b080da8
Ibalar <ibalartutby@gmail.com>
2026-04-20 22:40:25 +0300

    #04_20.04.26 update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,7 +54,6 @@
     @endif
 
     <!-- Collection -->
-
     <div class="themesFlat">
         <div class="container">
             <div dir="ltr" class="swiper tf-swiper" data-preview="3" data-tablet="2" data-mobile-sm="2"
